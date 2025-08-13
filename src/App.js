8af67cd3a4493e9ce71ea7779db757addd2607cd
Omee-Yehme <omingale786@gmail.com>
2025-08-13 16:49:33 +0530

    "Initial Commit"

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+import News from './News'
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <News/>
+      </div>
+    )
+  }
+}
